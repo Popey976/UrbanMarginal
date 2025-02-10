8100b26ae7ec2cb5ba3b92083acf0c4791770ed2
Adina Abdou salam <adina.aabdousalam976@hotmail.com>
2025-02-10 03:42:48 +0100

    Rajout de la méthode deconnexion dans JeuClient

--- src/JeuClient.java
+++ src/JeuClient.java
@@ -2,27 +2,42 @@
  * Gestion du jeu côté client
  *
  */
-public abstract class JeuClient extends Jeu {
+public abstract class JeuClient extends Jeu 
+{
 	
 	/**
 	 * Controleur
 	 */
-	public JeuClient() {
+	public JeuClient() 
+	{
+		
 	}
 	
 	@Override
-	public void connexion() {
+	public void connexion() 
+	{
+		
 	}
 
 	@Override
-	public void reception() {
+	public void reception() 
+	{
+		
 	}
 	
 	/**
 	 * Envoi d'une information vers le serveur
 	 * fais appel une fois à l'envoi dans la classe Jeu
 	 */
-	public void envoi() {
+	public void envoi() 
+	{
+		
+	}
+	
+	@Override
+	public void deconnexion() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
